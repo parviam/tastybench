15957c56edb0596bee814f776ef34cdf5d048516
Parv Mahajan <parv.mahajan@gtri.gatech.edu>
2025-11-10 18:38:12 -0500

    fixed data

--- model-elicitation/compare.py
+++ model-elicitation/compare.py
@@ -88,6 +88,7 @@ def compare_ranking_correlation(df1, df2, label1, target1, label2, target2, outp
     
     return results
 
+
 model_dirs = [
     'model-elicitation/data/curated/gpt-oss-20b/20-epochs',
     'model-elicitation/data/curated/gpt-oss-20b/50-epochs',
@@ -112,11 +113,11 @@ model_dirs = [
 for model_dir in tqdm(model_dirs):
     compare_ranking_correlation(
         df1=pd.read_csv(model_dir + '/elo.csv'),
-        df2=pd.read_csv('model-elicitation/data/llm_rl.csv'),
+        df2=pd.read_csv('model-elicitation/data/llm_rl_yix_curate.csv'),
         label1='paper_id',
         target1='elo_rating',
         label2='paperId',
         target2='b',
         title=model_dir.split('/')[-2],
-        output_dir=model_dir
+        output_dir=model_dir + '/'
     )
